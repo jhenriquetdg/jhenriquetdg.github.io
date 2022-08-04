ac23d8d4cf1434c8e9b7d9268653063323a78602
José Henrique Targino Dias Gois <jhenriquetdg@gmail.com>
2022-08-03 23:07:33 -0300

    Added content to the portifolio inprogress and curriculum

--- src/pages/Curriculum.jsx
+++ src/pages/Curriculum.jsx
@@ -1,5 +1,83 @@
-export default function Curriculum(){
-    return (
-        <h1>Curriculum</h1>
-    )
+import { X } from "phosphor-react";
+import { Chrono } from "react-chrono";
+
+const items = [
+  {
+    title: "May 1940",
+    cardTitle: "Bachelor",
+    url: "#",
+    cardSubtitle: "Bachalor in Science and Technology",
+    cardDetailedText: "Studied calculus, physics and neuroscience",
+    media: {
+      name: "Diploma BCT",
+      type: "IMAGE",
+      source: {
+        url: "/src/assets/diploma.jpg",
+      },
+    },
+  },
+  {
+    title: "May 1940",
+    cardTitle: "Master",
+    url: "#",
+    cardSubtitle: "Bachalor in Science and Technology",
+    cardDetailedText: "Studied calculus, physics and neuroscience",
+    media: {
+      name: "Diploma BCT",
+      type: "IMAGE",
+      source: {
+        url: "/src/assets/diploma.jpg",
+      },
+    },
+  },
+  {
+    title: "May 1940",
+    cardTitle: "Bachelor",
+    url: "#",
+    cardSubtitle: "Bachalor in Science and Technology",
+    cardDetailedText: "Studied calculus, physics and neuroscience",
+    media: {
+      name: "Diploma BCT",
+      type: "IMAGE",
+      source: {
+        url: "/src/assets/diploma.jpg",
+      },
+    },
+  },
+];
+
+export default function Curriculum() {
+  return (
+    <>
+      <div className="w-full h-full bg-zinc-300 flex flex-col items-center p-4">
+        <h1 className="text-orange-400 text-5xl border-b-2 border-b-orange-400 w-full text-center ">
+          Curriculum
+        </h1>
+        <div className="w-full">
+          <h2 className="text-orange-400 text-3xl border-b-2 border-b-orange-400 ">
+            Academic
+          </h2>
+          <Chrono
+            className="text-orange-400 h-24"
+            items={items}
+            mode="VERTICAL_ALTERNATING"
+            theme={{
+              primary: "orange",
+              secondary: "orange",
+              cardBgColor: "#52525b",
+              cardForeColor: "orange",
+              titleColor: "orange",
+              titleColorActive: "red",
+            }}
+          >
+            <div className="chrono-icons">
+              <img src="/src/assets/student.svg"></img>
+              <img src="/src/assets/logo.svg"></img>
+              <img src="/src/assets/logo.svg"></img>
+            </div>
+          </Chrono>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/services/scholar/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const api_scholar = axios.create({
+  baseURL:
+    "https://api.semanticscholar.org/graph/v1/author/51901962?fields=authorId,externalIds,url,name,aliases,affiliations,homepage,paperCount,citationCount,hIndex,papers.paperId,papers.externalIds,papers.url,papers.title,papers.abstract,papers.venue,papers.year,papers.referenceCount,papers.citationCount,papers.influentialCitationCount,papers.isOpenAccess,papers.fieldsOfStudy,papers.s2FieldsOfStudy,papers.publicationTypes,papers.publicationDate,papers.journal,papers.authors",
+});
