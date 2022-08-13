c582615216a054cd1f2b43c315feedd427e2f3b6
José Henrique Targino Dias Gois <jhenriquetdg@gmail.com>
2022-08-13 14:58:23 -0300

    Slim publication list

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -16,34 +16,52 @@ export default function Publications() {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1>
-        <a href={publications.url}> Publications</a>
+      <h1 className="text-orange-400 text-5xl border-b-2 border-b-orange-400 w-full text-center mt-10">
+        Publications
       </h1>
+
+      <h2 className="text-orange-400 text-3xl border-b-2 border-b-orange-400 my-10">
+        <a href={publications.url}> Scientific</a>
+      </h2>
+
       {/* { publications ? () : (<span></span)} */}
-      {publications?.papers?.map((paper) => {
+      {publications?.papers?.map((paper, p) => {
         return (
-          <div className="bg-slate-500 w-[90%] p-8 mb-4 rounded-2xl">
+          <div
+            key={p}
+            className="bg-slate-500 w-[90%] p-8 mb-4 rounded-2xl text-xs"
+          >
             <div className="flex flex-row justify-between my-2">
-              <span>{paper.journal.name}</span>
-              <a href={"http://" + paper.externalIds.DOI}>
-                {paper.externalIds.DOI}
+              <a
+                target="_blank"
+                href={"https://doi.org/" + paper.externalIds.DOI}
+              >
+                <span className="font-bold text-black">
+                  {paper.journal.name} : {paper.externalIds.DOI}
+                </span>
               </a>
-            </div>
-            <div className="flex flex-row justify-between">
-              <a href={paper.url}>
-                <h2>
-                  {paper.year} - {paper.title}
-                </h2>
-              </a>
+
               <span>Citations: {paper.citationCount}</span>
             </div>
 
+            <div className="flex flex-row justify-between">
+              <a href={paper.url} target="_blank">
+                <h2 className="text-orange-500 text-bold text-2xl">
+                  {paper.year} - {paper.title}
+                </h2>
+              </a>
+            </div>
+
             <div className="my-2">
-              {paper.authors.map((author) => {
-                return <span className="mr-4">{author.name}</span>;
+              {paper.authors.map((author, a) => {
+                return (
+                  <span key={a} className="mr-4 text-xs">
+                    {author.name}
+                  </span>
+                );
               })}
             </div>
-            <p>{paper.abstract}</p>
+            {/* <p className="text-justify text-sm">{paper.abstract}</p> */}
           </div>
         );
       })}
